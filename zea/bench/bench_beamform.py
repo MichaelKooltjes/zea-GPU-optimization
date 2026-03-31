0007259015936518acb15f551bcbd2d2051a903b
MichaelKooltjes <michael.kooltjes@gmail.com>
2026-03-31 17:37:03 +0200

    final change to benchmark

--- zea/bench/bench_beamform.py
+++ zea/bench/bench_beamform.py
@@ -1,9 +1,14 @@
 import os
 import time
 
+
+
 os.environ["KERAS_BACKEND"] = "jax"
 os.environ["ZEA_DISABLE_CACHE"] = "0"
 os.environ["ZEA_LOG_LEVEL"] = "INFO"
+
+import jax
+# jax.config.update("jax_disable_jit", True)
 
 import zea
 import numpy as np
@@ -11,30 +16,31 @@ from zea import init_device
 import jax.profiler
 from pathlib import Path
 
-# n_frames = 25
-# n_transmits = 10
+n_frames = 25
+n_transmits = 10
 
 init_device(verbose=False)
 
 with zea.File("hf://zeahub/zea-rotating-disk/L115V_1radsec.hdf5") as file:
     scan = file.scan()
 
-    # selected_tx = np.linspace(0, scan.n_tx_total - 1, n_transmits, dtype=int)
-    # selected_frames = slice(n_frames)
-    # scan.set_transmits(selected_tx)
+    selected_tx = np.linspace(0, scan.n_tx_total - 1, n_transmits, dtype=int)
+    selected_frames = slice(n_frames)
+    scan.set_transmits(selected_tx)
 
-    data = file.load_data("raw_data")
+    # data = file.load_data("raw_data")
+    data = file.load_data("raw_data", indices=(selected_frames, selected_tx))
     probe = file.probe()
 
 # beamform 
 beamform = zea.ops.Beamform(
     beamformer="delay_and_sum",
-    num_patches=200,
+    num_patches=2,
 )
 
 params = beamform.prepare_parameters(probe, scan)
 
-trace_dir = "/data/jax_traces/beamform_only"
+trace_dir = "/data/jax_traces/z_beamform_profiling"
 Path(trace_dir).mkdir(parents=True, exist_ok=True)
 
 # warmup
